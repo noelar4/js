14ceb3a2d7a3383d5cc45861d5c1b47d85ce1422
noelabraham <noelar4@gmail.com>
2020-06-05 11:11:05 -0400

    starting  commit

--- javascriptfile.js
+++ javascriptfile.js
@@ -1,14 +1,18 @@
 
 
 function firstfunction() { 
-    document.getElementById("disp").disp = "inside the function"
+  document.getElementById("disp").innerHTML = "Hello JavaScript!";
+
+  document.getElementById("calc_btn").style="display:none"
   } 
 
   function firstsumfunction() { 
-var sum = 0;
-for(var i=1000; i >= 17; i=i-11){
-    sum += i;
-}
-console.log(sum);
+    var sum = 0;
+    for(var i=1000; i >= 17; i=i-11){
+      sum += i;
+    }
+    console.log(sum);
+    document.getElementById("disp").innerHTML = sum;
+    document.getElementById("calc_btn").style="display:none"
   }
 //alert(sum);
