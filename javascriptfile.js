15c7fb81cbdfcc524c6f7ea117f8f09afbf01e4a
noelabraham <noelar4@gmail.com>
2020-06-05 10:56:30 -0400

    first commit

---
+++ javascriptfile.js
@@ -0,0 +1,14 @@
+
+
+function firstfunction() { 
+    document.getElementById("disp").disp = "inside the function"
+  } 
+
+  function firstsumfunction() { 
+var sum = 0;
+for(var i=1000; i >= 17; i=i-11){
+    sum += i;
+}
+console.log(sum);
+  }
+//alert(sum);
